93846d94ebd54fd3c563b83b36f1795d5665f9dd
Amaury1504v2 <57793398+amaury1504v2@users.noreply.github.com>
2024-03-07 11:55:53 +0400

    Update

---
+++ update.php
@@ -0,0 +1,55 @@
+<?php 
+include 'connect.php';
+$id=$_GET["updateid"];
+if (isset($_POST['update'])){ 
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobile'];
+    $password=$_POST['password'];
+
+    $sql="update `crud` set id=$id, name='$name', email='$email', mobile='$mobile', password='$password' where id='$id'";
+    $result=mysqli_query($con, $sql);
+    if($result){
+        //echo "Data inserted successfully";
+        header('location:display.php'); 
+    }else{
+        die(mysqli_error($con));
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container my-5">
+        <form method="post">
+            <div class="mb-3">
+                <label>Name</label>
+                <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label>Mobile</label>
+                <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Enter your password" name="password" autocomplete="off">
+            </div>
+            <button type="submit" class="btn btn-primary" name="update">Update</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </body>
+</html>
